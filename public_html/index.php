9649f345ce3add11fd6894c54bfa5d79b0ac92b5
shakevo <monochrome0329@gmail.com>
2024-07-22 15:48:39 +0900

    add noindex

--- public_html/index.php
+++ public_html/index.php
@@ -3,6 +3,7 @@
 <head>
     <!-- meta -->
     <meta charset="UTF-8">
+    <meta name="robots" content="noindex">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="WEBサイトのディスクリプション"/>
     <!-- ページタイトル -->
