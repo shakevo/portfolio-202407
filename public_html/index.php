d7356a8c273d45a81319cee14bbf7c6adc436fcb
EC2 Default User <ec2-user@ip-172-31-45-106.ap-northeast-1.compute.internal>
2024-07-22 02:50:11 +0000

    デザイン変更,自作テンプレート適用

--- public_html/index.php
+++ public_html/index.php
@@ -1,59 +1,129 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+    <!-- meta -->
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <title>shakevo - しゃけほっけ</title>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="WEBサイトのディスクリプション"/>
+    <!-- ページタイトル -->
+    <title>WEBサイトのタイトル</title>
+    <!-- WEBフォント -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1:wght@100..900&display=swap" rel="stylesheet">
+    <!-- css -->
+    <link rel="stylesheet" href="styles.css">
+    <!-- faviconのリンク -->
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
 </head>
-<body>
+<body id="top">
     <header>
-        <a href="#"><h1>しゃけほっけ(shakevo)</h1></a>
+        <!-- ヘッダーのロゴ画像リンク -->
+        <a href="https://bloom-t.co.jp/" target="_blank">
+            <img src="img/example.png" alt="ロゴ画像">
+        </a>
+        <span class="header-text">エンジニア採用</span>
+        <!--
+        ナビゲーション付けたくなったらこれを使う(記述例)
+        <nav>
+            <ul>
+                <li><a href="#introduction">はじめに</a></li>
+                <li><a href="#environment">開発環境</a></li>
+                <li><a href="#values">バリュー</a></li>
+                <li><a href="#benefits">福利厚生</a></li>
+                <li><a href="#jobs">求人情報</a></li>
+            </ul>
+        </nav>
+        -->
     </header>
-
     <main>
-        <section class="portfolio">
-            <a href="https://sujico.net/app/precure/stp_timer/" class="card-link">
-                <div class="card">
-                    <h2>ああああああ</h2>
-                    <p>テストテストテストテスト</p>
-                    <p>2024.05.22</p>
+        <section id="ccopy">
+            <h1>キャッチコピーH1</h1>
+        </section>
+        <section id="introduction">
+            <p>「はじめに」的な文章を入力</p>
+            <p>「はじめに」的な文章を入力</p>
+            <!-- アコーディオン -->
+            <div class="accordion">
+                <div class="accordion-item">
+                    <button id="accordion-button-1" aria-expanded="false">
+                        <span class="accordion-title">アコーディオンのタイトル</span>
+                        <span class="icon" aria-hidden="true"></span>
+                    </button>
+                    <div class="accordion-content">
+                        <p>アコーディオンの中身</p>
+                    </div>
                 </div>
-            </a>
-            <a href="#" class="card-link">
-                <div class="card">
-                    <h2>おおおおおおおおおお</h2>
-                    <p>テストテストテストテストテストテスト</p>
-                    <p>2022.03.13</p>
-                </div>
-            </a>
+                <br>
+            </div>
+            <a href="#top">ページトップに戻る</a>
+        </section>
+        <section id="environment">
+            <h2>大きい見出しH2</h2>
+            <h3>小さい見出しH3</h3>
+            <p>本文本文本文本文本文本文本文本文本文本文本文本文本文本文</p>
+            <h2>大きい見出しH2</h2>
+            <h3>小さい見出しH3</h3>
+            <p>本文本文本文本文本文本文本文本文本文本文本文本文本文本文</p>
+            <h2>大きい見出しH2</h2>
+            <h3>小さい見出しH3</h3>
+            <p>本文本文本文本文本文本文本文本文本文本文本文本文本文本文</p>
+            <a href="#top">ページトップに戻る</a>
+        </section>
+        <section id="devel">
+            <h2>大きい見出しH2</h2>
+            <h3>小さい見出しH3</h3>
+            <p>本文本文本文本文本文本文本文本文本文本文本文本文本文本文</p>
+            <img src="img/example.png" alt="画像呼び出し例(width100%にする)" width="100%">
+            <a href="#top">ページトップに戻る</a>
+        </section>
+        <section id="model">
+            <h2>大きい見出しH2</h2>
+            <!-- bold-textクラスは01から順番に大きい太字の文字になるスタイル -->
+            <h3>span class bol-textの利用例</h3>
+            <p>本文本文本文本文本文<span class="bold-text01">私たちは常に入社初日の気持ちで居続けます本文本文本文本文本文</span>本文本文本文本文</p>
+            <p>本文本文本文本文本文<span class="bold-text02">私たちは常に入社初日の気持ちで居続けます本文本文本文本文本文</span>本文本文本文本文</p>
+            <p>本文本文本文本文本文<span class="bold-text03">私たちは常に入社初日の気持ちで居続けます本文本文本文本文本文</span>本文本文本文本文</p>
+            <a href="#top">ページトップに戻る</a>
+        </section>
+        <section id="values">
+            <h2>大きい見出しH2</h2>
+            <h3>小さい見出しH3</h3>
+            <p>本文本文本文本文本文本文本文本文本文本文本文本文本文本文</p>
+            <a href="#top">ページトップに戻る</a>
+        </section>
+        <section id="evaluation">
+            <h2>大きい見出しH2</h2>
+            <h3>小さい見出しH3</h3>
+            <p>本文本文本文本文本文本文本文本文本文本文本文本文本文本文</p>
+            <a href="#top">ページトップに戻る</a>
+        </section>
+        <section id="benefits">
+            <h2>大きい見出しH2</h2>
+            <p>リストも使える、リストの利用例</p>
+            <h3>小さい見出しH3</h3>
+            <p>
+                <h3>休暇・働き方</h3>
+                <ul>
+                    <li>リスト</li>
+                    <li>リスト</li>
+                    <li>リスト</li>
+                    <li>リスト</li>
+                    <li>リスト</li>
+                </ul>
+            </p>
+            <a href="#top">ページトップに戻る</a>
+        </section>
+        <section id="jobs">
+            <h2>大きい見出しH2</h2>
+            <h3>小さい見出しH3</h3>
+            <p>本文本文本文本文本文本文本文本文本文本文本文本文本文本文</p>
+            <a href="#top">ページトップに戻る</a>
         </section>
     </main>
-
     <footer>
-
-    <center><a href="https://x.com/shakevo_/">しゃけほっけのTwitter</a></center>
-
-<!--
-SNSシェアボタン
-        <div class="share-buttons">
-            <a href="#" class="share-link" data-platform="twitter">
-                <img src="twitter-icon.png" alt="Twitter">
-            </a>
-            <a href="#" class="share-link" data-platform="instagram">
-                <img src="instagram-icon.png" alt="Instagram">
-            </a>
-            <a href="#" class="share-link" data-platform="facebook">
-                <img src="facebook-icon.png" alt="Facebook">
-            </a>
-            <a href="#" class="share-link" data-platform="line">
-                <img src="line-icon.png" alt="LINE">
-            </a>
-	</div>
--->
+        <p>&copy; 2024 shakevo</p>
     </footer>
     <script src="scripts.js"></script>
 </body>
 </html>
-
--- public_html/scripts.js
+++ public_html/scripts.js
@@ -1,30 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const shareLinks = document.querySelectorAll('.share-link');
+document.addEventListener("DOMContentLoaded", function() {
+    // セクション内のページ内リンクを取得
+    const links = document.querySelectorAll('section a[href^="#"]');
 
-    shareLinks.forEach(link => {
+    links.forEach(link => {
         link.addEventListener('click', function(event) {
+            // 既定のリンク動作をキャンセル
             event.preventDefault();
-            const platform = this.getAttribute('data-platform');
-            const url = window.location.href;
-            let shareUrl = '';
+            // ターゲットIDを取得
+            const targetId = event.target.getAttribute('href').substring(1);
+            // ターゲットIDを元にターゲットセクションを取得
+            const targetSection = document.getElementById(targetId);
 
-            switch(platform) {
-                case 'twitter':
-                    shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}&text=${encodeURIComponent('こんにちは、すじこバニラおにぎり')}`;
-                    break;
-                case 'instagram':
-                    shareUrl = `https://www.instagram.com/?url=${encodeURIComponent(url)}`;
-                    break;
-                case 'facebook':
-                    shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
-                    break;
-                case 'line':
-                    shareUrl = `https://line.me/R/msg/text/?${encodeURIComponent('こんにちは、すじこバニラおにぎり' + url)}`;
-                    break;
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // アコーディオンボタンを取得
+    const accordionButtons = document.querySelectorAll('.accordion button');
+
+    accordionButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            // 展開状態を取得
+            const expanded = this.getAttribute('aria-expanded') === 'true' || false;
+            // 展開状態を切り替える
+            this.setAttribute('aria-expanded', !expanded);
+
+            // ボタンの要素(コンテンツ)を取得
+            const content = this.nextElementSibling;
+            if (!expanded) {
+                // 展開する場合
+                content.style.display = 'block';
+            } else {
+                // 閉じる場合
+                content.style.display = 'none';
             }
-
-            window.open(shareUrl, '_blank');
         });
     });
 });
-
